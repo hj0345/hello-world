d75f8f6089590649b8dc2a69f6e1a95a9dd918b4
hj0345 <hjyang0345@gmail.com>
2019-05-11 13:29:13 +0900

    v0.2

--- play.py
+++ play.py
@@ -9,6 +9,8 @@ while True:
     b_color = (0, 255, 0)
     b_x1y1 = (100, 100)
     b_x2y2 = (300, 300)
+
+    a=1
     ret, frame = capture.read()
     frame = cv2.resize(frame, dsize=(o_width, o_height), interpolation=cv2.INTER_AREA)
     cv2.rectangle(frame, (100, 100), (300, 300), b_color, 3)
