ac90609ec340e8ee755c81e73aa5e70445941237
hj0345 <50508891+hj0345@users.noreply.github.com>
2019-05-11 13:05:50 +0900

    v.01
    afdfsfdf

---
+++ play.py
@@ -0,0 +1,20 @@
+import cv2
+capture = cv2.VideoCapture("dataset/Dry+Ice.mp4")
+while True:
+    if(capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT)):
+        capture.open("dataset/Dry+Ice.mp4")
+
+    o_height = 480
+    o_width = 640
+    b_color = (0, 255, 0)
+    b_x1y1 = (100, 100)
+    b_x2y2 = (300, 300)
+    ret, frame = capture.read()
+    frame = cv2.resize(frame, dsize=(o_width, o_height), interpolation=cv2.INTER_AREA)
+    cv2.rectangle(frame, (100, 100), (300, 300), b_color, 3)
+    cv2.imshow("VideoFrame", frame)
+
+    if cv2.waitKey(33) > 0: break
+
+capture.release()
+cv2.destroyAllWindows()
